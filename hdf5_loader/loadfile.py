da251b11da183090ba59ed8c5c119947979e984e
Ben <benh.schneider@gmail.com>
2015-09-17 01:02:36 +0200

    FW-BW sweep was a little mixed

--- hdf5_loader/loadfile.py
+++ hdf5_loader/loadfile.py
@@ -2,8 +2,8 @@ import numpy as np
 from parsers import load_hdf5, dim
 from parsers import savemtx, make_header
 # import matplotlib.pyplot as plt
-from changeaxis import interp_y
-from scipy.constants import Boltzmann as Kb
+# from changeaxis import interp_y
+# from scipy.constants import Boltzmann as Kb
 from scipy.constants import h, e, pi
 
 # filein = "S1_511_shot_100mV_4924_5217MHz"
@@ -11,10 +11,12 @@ from scipy.constants import h, e, pi
 # filein = 'S1_478_DCE_MAP_large_coarse'
 # filein = 'S1_514_DCE_MAP_high_pow'
 # folder = "hdf5s//09//Data_0915//"
-filein = 'S1_515_DCE_MAP_low_pow'
-folder = "hdf5s//09//Data_0916//"
+# filein = 'S1_515_DCE_MAP_low_pow'
+# folder = "hdf5s//09//Data_0916//"
 # filein = 'S1_515_DCE_MAP_700mV_pow'
 # filein = 'S1_514_DCE_MAP_low_pow'
+folder = "hdf5s//09//Data_0912//"
+filein = 'S1_482_shot_5090_5019MHz'
 d = load_hdf5(folder+filein+'.hdf5')
 
 
@@ -24,14 +26,17 @@ def get_MP(d, chnum):
     phase = np.unwrap(zip(*np.angle(compx)))
     return np.abs(compx), zip(*phase)
 
-MAT1 = np.zeros([8, d.shape[0], d.shape[1]])
-MAT1[0] = d.data[:, 2, :]
-MAT1[1] = d.data[:, 3, :]
-MAT1[2], MAT1[3] = get_MP(d, 8)
-MAT1[4], MAT1[5] = get_MP(d, 10)
-MAT1[6] = d.data[:, 12, :]
-MAT1[7] = d.data[:, 13, :]
 
+MAT1 = np.zeros([9, d.shape[0], d.shape[1]])
+MAT1[0] = d.data[:, 5, :]
+MAT1[1] = d.data[:, 6, :]
+MAT1[2], MAT1[3] = get_MP(d, 7)
+MAT1[4], MAT1[5] = get_MP(d, 9)
+MAT1[6] = d.data[:, 13, :]
+MAT1[7] = d.data[:, 11, :]
+MAT1[8] = d.data[:, 12, :]
+
+'''
 # scale data to photon number
 f1 = 4.924e9
 f2 = 5.217e9
@@ -61,7 +66,8 @@ d.n3.start = d.n3.lin[0]
 d.n3.stop = d.n3.lin[-1]
 d.n3.name = 'Flux/Flux0'
 
-header1 = make_header(d.n3, d.n2, d.n1, meas_data=('Photons [#]'))
+# header1 = make_header(d.n3, d.n2, d.n1, meas_data=('Photons [#]'))
+header1 = make_header(d.n3, d.n2, d.n1, meas_data=('Pow [W]'))
 savemtx('mtx_out//' + filein + '.mtx', MAT1, header=header1)
 
 factor = 10
@@ -79,17 +85,18 @@ d.dim_y2.name = 'Pump power (W)'
 
 header2 = make_header(d.n1, d.dim_y2, d.n3, meas_data='Photons [#]')
 savemtx('mtx_out//' + filein + 'W' + '.mtx', MAT2, header=header2)
-
 '''
+
 # this is used if a forward and backward sweep was used...
-n = 1
-d.n3 = [dim(name=d.stepInst[n],
+n = 0  # channel number of that instrument with multiple sweeps
+d.n2 = [dim(name=d.stepInst[n],
             start=sPar[3],
             stop=sPar[4],
             pt=sPar[8],
             scale=1)
         for sPar in d.stepItems[n]]
 
+'''
 d.n2[0].lin =  (d.n2[0].lin-xoff)/x1flux + 0.5
 d.n2[0].start = d.n2[0].lin[0]
 d.n2[0].stop =  d.n2[0].lin[-1]
@@ -98,14 +105,14 @@ d.n2[1].lin =  (d.n2[1].lin-xoff)/x1flux + 0.5
 d.n2[1].start = d.n2[1].lin[0]
 d.n2[1].stop =  d.n2[1].lin[-1]
 d.n2[1].name = 'Flux/Flux0'
+'''
 
 M2 = np.zeros((MAT1.shape[0], d.n2[0].pt, d.n3.pt))
 M3 = np.zeros((MAT1.shape[0], d.n2[1].pt, d.n3.pt))
-M3 = MAT1[:, :d.n2[0].pt, :]
-M2 = MAT1[:, d.n2[0].pt-1:, :]
+M2 = MAT1[:, :d.n2[0].pt, :]
+M3 = MAT1[:, d.n2[0].pt-1:, :]
 
-header2 = make_header(d.n3, d.n2[0], d.n1, meas_data=('a.u.'))
-header1 = make_header(d.n3, d.n2[1], d.n1, meas_data=('a.u.'))
-savemtx('mtx_out//' + filein + '.mtx', M3, header=header1)
-savemtx('mtx_out//' + filein + '2' + '.mtx', M2, header=header2)
-'''
+header1 = make_header(d.n3, d.n2[0], d.n1, meas_data=('a.u.'))
+savemtx('mtx_out//' + filein + '.mtx', M2, header=header1)
+header2 = make_header(d.n3, d.n2[1], d.n1, meas_data=('a.u.'))
+savemtx('mtx_out//' + filein + '2' + '.mtx', M3, header=header2)
