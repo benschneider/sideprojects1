d83178371852ac2a635f9f57d1d00702e99bee93
Ben <benh.schneider@gmail.com>
2015-10-24 17:30:21 +0200

    Rudimentary fitting of the function, yet the fit is less than perfect

--- shotnoise/fitshot.py
+++ shotnoise/fitshot.py
@@ -1,6 +1,6 @@
 import numpy as np
 from parsers import savemtx, loadmtx
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, leastsq
 from scipy.constants import Boltzmann as Kb
 from scipy.constants import h, e  # , pi
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ Zopt = 50.0
 T = 0.012
 R = 75.0
 Tn = 3.1
-B = 50e6
+B = 1e6
 f = 5800*1e6
 G = 59627515
 RTR = 1009.1  # Resistance at RT in kOhm in front of V output
@@ -20,13 +20,25 @@ folder = 'mtx_out//'
 filein = 'S1_631_SN_G100_BPF4'
 M1, d1, d2, d3, dz = loadmtx('mtx_out//' + filein + '.mtx')
 
-d1.scale = 1000.0/RTR  # scale to µAmp
+d1.scale = 1000.0/RTR  # scale to uAmp
 d1.update_lin()
-
 IVs = M1[8]
-IVs = IVs * 1000.0 # scale to µVolts
+IVs = IVs * 1000.0  # scale to uVolts
 PD1 = M1[0]
 PD2 = M1[1]
+
+# 200, 2300
+# crop data to be fitted
+d1.lin2 = d1.lin[200:2300]
+d1.pt = 2300-200
+IVs2 = IVs[:, 200:2300]
+PD12 = PD1[:, 200:2300]
+PD22 = PD2[:, 200:2300]
+
+d1.lin = d1.lin2
+IVs = IVs2
+PD1 = PD12
+PD2 = PD22
 
 
 def xderiv(d2MAT, dx=1.0, axis=0):
@@ -59,6 +71,8 @@ def xderiv(d2MAT, dx=1.0, axis=0):
 dx1 = d1.lin[1] - d1.lin[0]
 dIV = xderiv(IVs, dx1)
 dIVm = np.mean(dIV, axis=0)
+plt.close()
+plt.figure(1)
 plt.plot(d1.lin, dIVm)
 
 
@@ -67,48 +81,37 @@ def find_nearest(someArray, value):
     return idx
 
 
-#Fitting functions:
+def Rm(x):
+    return np.abs(dIVm[x])
 
 
-def Rm(I):
-    # pos = find_nearest()
-    return dIV[I]
-    # return 75.0  # temp solution for now
-    # Rm(R) = abs(R)>200.0 ? 100.0 : abs(R)<1.0 ? 5.0 : abs(R)
+def fitfunc(x, G, Tn, T):
+    R = Rm(x)
+    I = d1.lin[x]*1e-6  # because needs to be in Amps here
+    E1 = (e*I*R+h*f)/(2*Kb*T)
+    E2 = (e*I*R-h*f)/(2*Kb*T)
+    Si = ((2*Kb*T/R) * (E1/np.tanh(E1) + E2/np.tanh(E2)))
+    return (B*G*(Si * Rm(x)**2 + 4.0*Kb*T*Rm(x)
+                 + 4.0*Kb*Tn*Z0*(Rm(x)**2 + Zopt*Zopt)/(Z0*Z0 + Zopt*Zopt))
+            * (Z0/((Rm(x)+Z0)*(Rm(x)+Z0))))
 
 
-def E1(I, R, T, f):
-    return (e*I*R+h*f)/(2*Kb*T)
+def fitfun2(x, G, Tn):
+    T = 0.012
+    return fitfunc(x, G, Tn, T)*1e9
 
+xdata = range(d1.pt)
+ydata = PD1[50]*1e9
+# initguess = [G, Tn, T]
+initguess = [G, Tn]
 
-def E2(I, R, T, f):
-    return (e*I*R-h*f)/(2*Kb*T)
+popt, pcov = curve_fit(fitfun2, xdata, ydata, p0=initguess)
+G2, Tn2 = popt
 
+y2 = fitfun2(xdata, G2, Tn2)
+plt.figure(2)
+plt.plot(d1.lin, ydata)
+plt.plot(d1.lin, y2)
 
-def Si(I, R, f, T):
-    return ((2*Kb*T/R)
-            * (E1(I, R, T, f)/np.tanh(E1(I, R, T, f))
-               + E2(I, R, T, f)/np.tanh(E2(I, R, T, f))))
-
-
-def func(I, G, Tn, T):
-    return (B*G*(Si(I, Rm(I), f, T) * Rm(I)**2 + 4.0*Kb*T*Rm(I)
-                 + 4.0*Kb*Tn*Z0*(Rm(I)**2 + Zopt*Zopt)/(Z0*Z0 + Zopt*Zopt))
-            * (Z0/((Rm(I)+Z0)*(Rm(I)+Z0))))
-
-xdata = np.linspace(-25e-6, 25e-6, 5001)
-y = func(xdata, G, Tn, T)
-ydata = y + 1e-7 * np.random.normal(size=len(xdata))
-
-
-initguess = [G, Tn, T]
-popt, pcov = curve_fit(func, xdata, ydata, p0=initguess)
-G2, Tn2, T2 = popt
-y2 = func(xdata, G2, Tn2, T2)
-
-plt.plot(xdata, ydata)
-plt.plot(xdata, y2)
-
-# fit [-19:-5][:] Pout4(x, y)  dFileN u ($1/14.27e-3):($5):($3) via G, Tn
 # header1 = make_header(d.n1, d.n2, d.n3, meas_data=('Pow [W]'))
 # savemtx('mtx_out//' + filein + '.mtx', MAT1, header=header1)
