cdd8b1536bbd724b03aaf346cfe4e6731e7e8445
Ben <benh.schneider@gmail.com>
2016-02-16 14:41:08 +0100

    thats a pyfile

--- shotnoise/S1_949_G0mV.py
+++ shotnoise/S1_949_G0mV.py
@@ -14,6 +14,7 @@ from scipy.constants import Boltzmann as Kb
 from scipy.constants import h, e  # , pi
 from scipy.ndimage.filters import gaussian_filter1d
 import matplotlib.pyplot as plt
+from lmfit import minimize, Parameters, report_fit  # , Parameter
 
 
 def xderiv(d2MAT, dx=1.0, axis=0):
@@ -63,36 +64,19 @@ def find_nearest(someArray, value):
     return idx
 
 
-def Rm(x, c=True):
-    ''' This function is used to handle the differential resistance values
-    since a differential resistance can be negative or infinite at single point
-    jumps. This definition requires two globals which contain the differential
-    resistance. dRm and dRm2.. I used this to test different ways of calculating
-    and obtaining differential resistances.
-    '''
-    if c is True:
-        return np.abs(dRm[x])
-
-
-def fitfunc(x, G, Tn, T, c):
+def fitfunc(I, R, G, Tn, T):
+    # def fitfunc(x, G, Tn, T, c):
     '''
     This contains the fitting equation, which i use to fit the
     shot noise response.
     returns: fit-value(x, ...)
     '''
-    R = Rm(x, c)
-    I = d1.lin[x]*1e-6  # because needs to be in Amps here
     E1 = (e*I*R+h*f)/(2*Kb*T)
     E2 = (e*I*R-h*f)/(2*Kb*T)
     Si = ((2*Kb*T/R) * (E1/np.tanh(E1) + E2/np.tanh(E2)))
     return (B*G*(Si * R**2 + 4.0*Kb*T*R + 4.0*Kb*Tn*Z0 *
                  (R**2+Zopt*Zopt)/(Z0*Z0+Zopt*Zopt)) *
             (Z0/((R+Z0)*(R+Z0))))
-
-
-def fitfun2(x, G, Tn, c=True):
-    T = 0.012
-    return fitfunc(x, G, Tn, T, c)*1e12
 
 
 '''
@@ -116,15 +100,10 @@ lags0 = find_nearest(d1.lin, 0.0)  # lags position
 PD1 = (I1I1[lags0]+Q1Q1[lags0])
 PD2 = (I2I2[lags0]+Q2Q2[lags0])
 
-# Roughly the difference between the two is roughly:
-print np.mean(PD1[0])/np.mean(PD2[0])
-# i.e. 0.779 ~ 28% difference
+print np.mean(PD1[0])/np.mean(PD2[0])  # i.e. 0.779 ~ 28% difference
 
 # Getting the differential Resistance
 # ------------------------------------
-# First get the units right,
-# Current in uA and voltage in uV
-# Then take the derivative
 # And Optionally filter discontinuities
 # RTR = 1009.1                  # Ib Resistance in kOhm
 # RG = 1000.0                   # Pre Amp gain factor
@@ -138,82 +117,65 @@ IVs = Vm[0, pidx]
 dx3 = dv3.lin[1] - dv3.lin[0]   # get step-size for the derivative
 dIV = xderiv(IVs, dx3)
 # dIVm = np.mean(dIV, axis=0)   # this was used to simply average several traces
-# dRm = gaussian_filter1d(dIV, 5)  # 5pt Gaussian filter avoid discontinuities
-dRm = abs(dIV)                  # no filter other than an absolute val for now
+dR = abs(dIV)                  # no filter other than an absolute val for now
+dRm = gaussian_filter1d(dR, 1.5)  # Gaussian filter
+# dRm = abs(dRm)                  # no filter other than an absolute val for now
 # ------------------------------------
 
 
-#  Plot data loaded so far
-plt.ion()
-plt.figure(1)
-plt.imshow(PD1, aspect='auto')
-plt.figure(2)
-plt.plot(PD1[0])
-plt.figure(3)
-plt.plot(d3.lin, abs(dRm))
-plt.show()
-
-# Fitting start parameters
 Z0 = 50.0
 Zopt = 50.0
-T = 0.012
-R = 75.0
-Tn = 3.1
-B = 1e6
-f = 5800*1e6
-G = 59627515
+B = 1e5
+f = 4100*1e6
+I = d3.lin*1e-6  # in Amps
+
+plt.ion()
+crop_within = find_nearest(I, -0.9e-6), find_nearest(I, 1.1e-6)
+# crop_within = find_nearest(I, -7.3e-6), find_nearest(I, 7.3e-6)
+crop_outside = find_nearest(I, -19.5e-6), find_nearest(I, 19.5e-6)
+crop = [crop_within, crop_outside]
+# crop_within = find_nearest(I, -4e-6), find_nearest(I, 4e-6)
+# crop = find_nearest(I, -2e-6), find_nearest(I, 2e-6), 2.3e-10, 5.6e-10
+crop2 = [crop_within, crop_outside]
 
 
-'''
-To obtain the differential resistance we need to take the derivative
-of the measured voltage with respect to its current.
-to take this derivative we use xderiv function :
-'''
-
-'''
-# initguess = [G, Tn, T]
-initguess1 = [G, Tn]
-initguess2 = [G, Tn]
+data = PD1[0]*1.0
+params = Parameters()
+params.add('Tn', value=3.7, vary=True, min=2, max=4)
+params.add('G', value=3.38e7, vary=True, min=1e6, max=1e9)
+params.add('T', value=0.012, vary=False)
 
 
-# crop data to be fitted
-start1 = 200
-stop1 = 800
-start2 = 1600
-stop2 = 2300
-dRm2 = dRm
-xdata2 = np.concatenate((range(start1, stop1), range(start2, stop2)), axis=0)
-PD12 = np.concatenate((PD2[:, start1:stop1], PD1[:, start2:stop2]), axis=1)
-PD22 = np.concatenate((PD2[:, start1:stop1], PD2[:, start2:stop2]), axis=1)
-numN = PD12.shape[0]
-G1G2Tn1Tn2 = np.zeros([numN, 4])
-
-i = 0
-for i in range(numN):
-    ydata1 = PD12[i]*1e12
-    ydata2 = PD22[i]*1e12
-    popt, pcov = curve_fit(fitfun2, xdata2, ydata1, p0=initguess1)
-    popt2, pcov2 = curve_fit(fitfun2, xdata2, ydata2, p0=initguess2)
-    initguess1 = popt  # for the next position
-    initguess2 = popt2
-    G1G2Tn1Tn2[i, 0], G1G2Tn1Tn2[i, 1] = popt
-    G1G2Tn1Tn2[i, 2], G1G2Tn1Tn2[i, 3] = popt2
-
-# def func(params) = ydata - fitfun2(xdata, params)
-# scipy.optimize.leastsq(
-
-ydata = PD1[50]*1e12
-xdata = range(PD1.shape[1])
-y2 = fitfun2(xdata, G1G2Tn1Tn2[50, 0], G1G2Tn1Tn2[50, 1], c=0)
-error = ydata - y2
-plt.figure(2)
-plt.plot(d1.lin, ydata)
-plt.plot(d1.lin, y2)
+def fitfun2(params, I, dRm):
+    G = params['G'].value
+    Tn = params['Tn'].value
+    T = params['T'].value
+    return fitfunc(I, dRm, G, Tn, T)
 
 
-MAT1 = np.zeros([1, G1G2Tn1Tn2.shape[0], G1G2Tn1Tn2.shape[1]])
-MAT1[0, :, :] = G1G2Tn1Tn2
-header1 = make_header(d1, d2, d3, meas_data=('Gain'))
-savemtx('mtx_out//' + filein1 + 'test.mtx', MAT1, header=header1)
+def ministuff(params, I, dRm, measd, crop):
+    SNfit = fitfun2(params, I, dRm)
+    # crop data for fitting:
+    # crop vertically
+    # SNfit[SNfit < crop[2]] = 0
+    # SNfit[SNfit > crop[3]] = 0
+    # measd[measd < crop[2]] = 0
+    # measd[measd > crop[3]] = 0
+    # crop horizontally anything too close to zero
+    SNfit[crop[0][0]:crop[0][1]] = 0
+    measd[crop[0][0]:crop[0][1]] = 0
+    SNfit[0:(crop[1][0])] = 0
+    SNfit[crop[1][1]:-1] = 0
+    measd[0:(crop[1][0])] = 0
+    measd[crop[1][1]:-1] = 0
+    return (measd-SNfit)*1e10
 
-'''
+
+result = minimize(ministuff, params, args=(I, dRm, data*1.0, crop))
+print report_fit(result)
+deltaTn = result.params['Tn'].stderr
+deltaG = result.params['G'].stderr
+SNfit = fitfun2(result.params, I, dRm)
+
+
+
