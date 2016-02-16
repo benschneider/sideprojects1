bd79a6f74e3add181bf288029ab68fa731d0ac73
Ben <benh.schneider@gmail.com>
2016-02-16 15:05:31 +0100

    add comments, and clean

--- shotnoise/S1_949_G0mV.py
+++ shotnoise/S1_949_G0mV.py
@@ -79,6 +79,44 @@ def fitfunc(I, R, G, Tn, T):
             (Z0/((R+Z0)*(R+Z0))))
 
 
+def fitfun2(params, I, dRm):
+    '''
+    req: params with G, Tn, T;
+         I (current array or value) (Amps)
+         dRm (Resistance for this current)
+    return: fitting value or array
+    '''
+    G = params['G'].value
+    Tn = params['Tn'].value
+    T = params['T'].value
+    return fitfunc(I, dRm, G, Tn, T)
+
+
+def ministuff(params, I, dRm, measd, crop):
+    '''
+    req: params with G, Tn, T
+         I (current array or value) (Amps)
+         dRm (Resistance for this current)
+         measured data (value or array)
+         crop values for example create with:
+         crop_within = find_nearest(I, -0.9e-6), find_nearest(I, 1.1e-6)
+         crop_outside = find_nearest(I, -19.5e-6), find_nearest(I, 19.5e-6)
+         crop = [crop_within, crop_outside]
+         This crop is used to cut data corresponding to the current values
+         i.e. to cut away the critical current part (from to (crop within))
+         also edges where the derivative and filter is inaccurate (crop outside)
+    returns: residuals*1e10;
+         (difference between measured and fitted data after it has been croped)
+    '''
+    SNfit = fitfun2(params, I, dRm)
+    SNfit[crop[0][0]:crop[0][1]] = 0
+    measd[crop[0][0]:crop[0][1]] = 0
+    SNfit[0:(crop[1][0])] = 0
+    SNfit[crop[1][1]:-1] = 0
+    measd[0:(crop[1][0])] = 0
+    measd[crop[1][1]:-1] = 0
+    return (measd-SNfit)*1e10
+
 '''
 Loading the data files I1I1, Q1Q1, I2I2, Q2Q2, Vm
 d1, d2, d3 are all the same since they all originate from the same type of
@@ -89,7 +127,7 @@ filein1 = 'S1_949_G0mV_SN_PCovMat_cI1I1'
 filein2 = 'S1_949_G0mV_SN_PCovMat_cQ1Q1'
 filein3 = 'S1_949_G0mV_SN_PCovMat_cI2I2'
 filein4 = 'S1_949_G0mV_SN_PCovMat_cQ2Q2'
-filein5 = 'S1_949_G0mV_SN_PV_2'
+filein5 = 'S1_949_G0mV_SN_PV'
 I1I1, d3, d2, d1, dz = loadmtx('sn_data//' + filein1 + '.mtx')
 Q1Q1, d3, d2, d1, dz = loadmtx('sn_data//' + filein2 + '.mtx')
 I2I2, d3, d2, d1, dz = loadmtx('sn_data//' + filein3 + '.mtx')
@@ -100,75 +138,46 @@ lags0 = find_nearest(d1.lin, 0.0)  # lags position
 PD1 = (I1I1[lags0]+Q1Q1[lags0])
 PD2 = (I2I2[lags0]+Q2Q2[lags0])
 
-print np.mean(PD1[0])/np.mean(PD2[0])  # i.e. 0.779 ~ 28% difference
-
-# Getting the differential Resistance
-# ------------------------------------
-# And Optionally filter discontinuities
-# RTR = 1009.1                  # Ib Resistance in kOhm
-# RG = 1000.0                   # Pre Amp gain factor
-# dv3.scale = 1e3/(RTR)         # scale to uAmp
-# dv3.update_lin()
-# IVs = IVs * 1e6/RG            # scale to uVolts
-# the axis and data scaling was done beforehand for this data set
-d3 = dv3                        # update all d3 axis
-pidx = 0                        # power index 0 is 0 power
-IVs = Vm[0, pidx]
-dx3 = dv3.lin[1] - dv3.lin[0]   # get step-size for the derivative
-dIV = xderiv(IVs, dx3)
-# dIVm = np.mean(dIV, axis=0)   # this was used to simply average several traces
-dR = abs(dIV)                  # no filter other than an absolute val for now
-dRm = gaussian_filter1d(dR, 1.5)  # Gaussian filter
-# dRm = abs(dRm)                  # no filter other than an absolute val for now
-# ------------------------------------
-
 
 Z0 = 50.0
 Zopt = 50.0
 B = 1e5
 f = 4100*1e6
-I = d3.lin*1e-6  # in Amps
+RTR = 1009.1 * 1e3           # Ib Resistance in Ohm
+RG = 1000.0                   # Pre Amp gain factor
+
+
+pidx = 0                        # power index 0 is 0 power
+mean_diff = np.mean(PD1[pidx])/np.mean(PD2[pidx])  # i.e. 0.779 ~ 28% difference
+# Getting the differential Resistance
+# -----------------------------------------
+
+d3.scale = 1/(RTR)         # scale to Amp
+d3.update_lin()
+IVs = Vm[0, pidx]
+IVs = IVs/RG            # scale to Volts
+
+dx3 = dv3.lin[1] - dv3.lin[0]   # get step-size for the derivative
+dIV = xderiv(IVs, dx3)
+dR = abs(dIV)                  # no filter other than an absolute val for now
+dRm = gaussian_filter1d(dR, 1.5)  # Gaussian filter
+# -----------------------------------------
+I = dv3.lin*1e-6  # in Amps
+
 
 plt.ion()
 crop_within = find_nearest(I, -0.9e-6), find_nearest(I, 1.1e-6)
-# crop_within = find_nearest(I, -7.3e-6), find_nearest(I, 7.3e-6)
 crop_outside = find_nearest(I, -19.5e-6), find_nearest(I, 19.5e-6)
 crop = [crop_within, crop_outside]
-# crop_within = find_nearest(I, -4e-6), find_nearest(I, 4e-6)
-# crop = find_nearest(I, -2e-6), find_nearest(I, 2e-6), 2.3e-10, 5.6e-10
-crop2 = [crop_within, crop_outside]
 
 
-data = PD1[0]*1.0
+data = PD1[pidx]*1.0
 params = Parameters()
 params.add('Tn', value=3.7, vary=True, min=2, max=4)
 params.add('G', value=3.38e7, vary=True, min=1e6, max=1e9)
 params.add('T', value=0.012, vary=False)
 
 
-def fitfun2(params, I, dRm):
-    G = params['G'].value
-    Tn = params['Tn'].value
-    T = params['T'].value
-    return fitfunc(I, dRm, G, Tn, T)
-
-
-def ministuff(params, I, dRm, measd, crop):
-    SNfit = fitfun2(params, I, dRm)
-    # crop data for fitting:
-    # crop vertically
-    # SNfit[SNfit < crop[2]] = 0
-    # SNfit[SNfit > crop[3]] = 0
-    # measd[measd < crop[2]] = 0
-    # measd[measd > crop[3]] = 0
-    # crop horizontally anything too close to zero
-    SNfit[crop[0][0]:crop[0][1]] = 0
-    measd[crop[0][0]:crop[0][1]] = 0
-    SNfit[0:(crop[1][0])] = 0
-    SNfit[crop[1][1]:-1] = 0
-    measd[0:(crop[1][0])] = 0
-    measd[crop[1][1]:-1] = 0
-    return (measd-SNfit)*1e10
 
 
 result = minimize(ministuff, params, args=(I, dRm, data*1.0, crop))
