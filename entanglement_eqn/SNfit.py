91d6ea9e1c7b821d94ebe6aae6d4f4dd232c32fb
Ben <benh.schneider@gmail.com>
2016-02-23 18:46:28 +0100

    Change in SN equation! and attempt to find photon Temp.

--- entanglement_eqn/SNfit.py
+++ entanglement_eqn/SNfit.py
@@ -5,153 +5,22 @@ A script is used to readout mtx measurement data which also contains
 a shotnoise responses.
 Then fits them for G and Tn
 '''
-
 import numpy as np
 from parsers import savemtx, loadmtx, make_header
 # from scipy.optimize import curve_fit  # , leastsq
 from scipy.constants import Boltzmann as Kb
-from scipy.constants import h, e  # , pi
+from scipy.constants import h, e, c  # , pi
 from scipy.ndimage.filters import gaussian_filter1d
 from lmfit import minimize, Parameters, report_fit  # , Parameter
 from matplotlib.pyplot import plot, hold, figure, show, title, ion, close
 
 
-def xderiv(d2MAT, dx=1.0, axis=0):
-    '''
-    This  derivative is inaccurate at the edges.
-    Calculates a 3p derivative of a 1D, 2D matrix.
-    This does not require you to shift the xaxis by one half pt.
-    dx = distance between points
-    '''
-    if len(d2MAT.shape) > 1:
-        if axis == 1:
-            ''' Not tested yet could be faster than a matrix transpose'''
-            a2 = np.zeros([d2MAT.shape[0]+2, d2MAT.shape[1]])
-            a2[1:-1, :] = d2MAT
-            m1 = d2MAT - a2[:-2, :]
-            m2 = a2[2:, :] - d2MAT
-            dy = (m1+m2)/2.0
-            dy[0, :] = dy[1, :]
-            dy[-1, :] = dy[-2, :]
-        elif axis == 0:
-            a2 = np.zeros([d2MAT.shape[0], d2MAT.shape[1]+2])
-            a2[:, 1:-1] = d2MAT
-            m1 = d2MAT - a2[:, :-2]
-            m2 = a2[:, 2:] - d2MAT
-            dy = (m1+m2)/2.0
-            dy[:, 0] = dy[:, 1]
-            dy[:, -1] = dy[:, -2]
-        return dy/dx
-    else:
-        a2 = np.zeros([d2MAT.shape[0]+2])
-        a2[1:-1] = d2MAT
-        m1 = d2MAT - a2[:-2]
-        m2 = a2[2:] - d2MAT
-        dy = (m1+m2)/2.0
-        dy[0] = dy[1]
-        dy[-1] = dy[-2]
-        return dy/dx
-
-
-def find_nearest(someArray, value):
-    ''' This function helps to find the index corresponding to a value
-    in an array.
-    Usage: indexZero = find_nearest(myarray, 0.0)
-    returns: abs(myarray-value).argmin()
-    '''
-    idx = abs(someArray-value).argmin()
-    return idx
-
-
-def fitfunc(G, Tn, T, vc):
-    # def fitfunc(x, G, Tn, T, c):
-    '''
-    This contains the fitting equation, which i use to fit the
-    shot noise response.
-    returns: fit-value(x, ...)
-    '''
-    E1 = (e*vc.I*vc.dRm+h*vc.f)/(2*Kb*T)
-    E2 = (e*vc.I*vc.dRm-h*vc.f)/(2*Kb*T)
-    Si = ((2*Kb*T/vc.dRm) * (E1/np.tanh(E1) + E2/np.tanh(E2)))
-    return (vc.B*G*(Si * vc.dRm**2 +
-                    4.0*Kb*T*vc.dRm + 4.0*Kb*Tn*vc.Z0 *
-                    (vc.dRm**2+vc.Zopt*vc.Zopt)/(vc.Z0*vc.Z0+vc.Zopt*vc.Zopt)) *
-            (vc.Z0/((vc.dRm+vc.Z0)*(vc.dRm+vc.Z0))))
-
-
-def fitfun2(params, vc, digi):
-    '''
-    req: params with G, Tn, T; and vc as variable carrier
-    return: fitting value or array
-    also set digi='D1' or digi='D2'
-    '''
-    if digi == 'D1':
-        G = params['G1'].value
-        Tn = params['Tn1'].value
-        T = params['T1'].value
-        vc.f = vc.f1
-    if digi == 'D2':
-        G = params['G2'].value
-        Tn = params['Tn2'].value
-        T = params['T2'].value
-        vc.f = vc.f2
-    return fitfunc(G, Tn, T, vc)
-
-
-def ministuff(params, vc, measd, digi='D1'):
-    '''
-    req: params with G, Tn, T
-         I (current array or value) (Amps)
-         dRm (Resistance for this current)
-         digi='D1' or 'D2'
-         measured data (value or array)
-         crop values for example create with:
-         crop_within = find_nearest(I, -0.9e-6), find_nearest(I, 1.1e-6)
-         crop_outside = find_nearest(I, -19.5e-6), find_nearest(I, 19.5e-6)
-         crop = [crop_within, crop_outside]
-         This crop is used to cut data corresponding to the current values
-         i.e. to cut away the critical current part (from to (crop within))
-         also edges where the derivative and filter is inaccurate (crop outside)
-    returns: residuals*1e10;
-         (difference between measured and fitted data after it has been croped)
-    '''
-    SNfit = fitfun2(params, vc, digi)
-    SNfit[vc.crop[0][0]:vc.crop[0][1]] = 0
-    measd[vc.crop[0][0]:vc.crop[0][1]] = 0
-    SNfit[0:(vc.crop[1][0])] = 0
-    SNfit[vc.crop[1][1]:-1] = 0
-    measd[0:(vc.crop[1][0])] = 0
-    measd[vc.crop[1][1]:-1] = 0
-    return (measd-SNfit)*1e10
-
-
-def bigstuff(params, vc, pidx):
-    '''
-    return all 3 as one combined thing (shotnoise 1,2 and photon differences)
-    '''
-    data1 = vc.cPD1[pidx]*1.0
-    data2 = vc.cPD2[pidx]*1.0
-    snd1 = ministuff(params, vc, data1, 'D1')
-    snd2 = ministuff(params, vc, data2, 'D2')
-    phtd = photondiff(params, data1, data2, vc)
-    return (abs(snd1) + abs(snd2))*phtd
-
-
-def photondiff(params, data1, data2, vc):
-    '''
-    calculates the difference in photon numbers at the hemt input
-    for a current bias of Ib0 (0 uA) and after the amplifier photons and temp
-    photons are subtracted.
-    '''
-    p1 = data1[vc.Ib0]
-    p2 = data2[vc.Ib0]
-    Ent1 = params['T1'].value*Kb
-    Ent2 = params['T2'].value*Kb
-    Amp1 = params['Tn1'].value*Kb
-    Amp2 = params['Tn2'].value*Kb
-    Phot1 = (p1-Amp1-Ent1)/h*vc.f1
-    Phot2 = (p2-Amp2-Ent2)/h*vc.f2
-    return (Phot1 - Phot2)*1e2
+'''
+The Two classes SN_class used to store the fit results
+------------------------------------------------------
+and the variable carrier used to carry and deal with variables that are
+passed over quite often
+'''
 
 
 class SN_class():
@@ -171,6 +40,8 @@ class SN_class():
         self.Tn2 = []
         self.T1 = []
         self.T2 = []
+        self.T = []
+        self.Tdel = []
 
 
 class variable_carrier():
@@ -264,6 +135,7 @@ class variable_carrier():
         '''
         self.d3step = self.d3.lin[1] - self.d3.lin[0]   # get step-size
         self.dIV = xderiv(self.Vm[0], self.d3step)
+        self.dIV[self.dIV > 100.0] = 50.0  # Replace any value larger than 200
         self.dIVlp = gaussian_filter1d(abs(self.dIV), self.LP)  # Gausfilter
 
     def make_cvals(self, cpt=5, snr=3):
@@ -338,15 +210,23 @@ class variable_carrier():
                 self.cvals['nI2I2'][x2, x3] = r2i[x2, x3]  # +u2i/2
                 self.cvals['nQ2Q2'][x2, x3] = r2q[x2, x3]  # +u2q/2
                 # that error is already added from the shot noise values
-                self.cvals['nI1I2'][x2, x3] = rii  # +uii/2 if rii < 0 else rii-uii/2
-                self.cvals['nI1Q2'][x2, x3] = riq  # +uiq/2 if riq < 0 else riq-uiq/2
-                self.cvals['nQ1I2'][x2, x3] = rqi  # +uqi/2 if rqi < 0 else rqi-uqi/2
-                self.cvals['nQ1Q2'][x2, x3] = rqq  # +uqq/2 if rqq < 0 else rqq-uqq/2
+                self.cvals['nI1I2'][x2, x3] = rii
+                # +uii/2 if rii < 0 else rii-uii/2
+                self.cvals['nI1Q2'][x2, x3] = riq
+                # +uiq/2 if riq < 0 else riq-uiq/2
+                self.cvals['nQ1I2'][x2, x3] = rqi
+                # +uqi/2 if rqi < 0 else rqi-uqi/2
+                self.cvals['nQ1Q2'][x2, x3] = rqq
+                # +uqq/2 if rqq < 0 else rqq-uqq/2
                 # 0 if the requested SnR ratio is not met
-                if abs(rii) < snr*uii/2.0: self.cvals['nI1I2'][x2, x3] = 0.0
-                if abs(riq) < snr*uiq/2.0: self.cvals['nI1Q2'][x2, x3] = 0.0
-                if abs(rqi) < snr*uqi/2.0: self.cvals['nQ1I2'][x2, x3] = 0.0
-                if abs(rqq) < snr*uqq/2.0: self.cvals['nQ1Q2'][x2, x3] = 0.0
+                if abs(rii) < snr*uii/2.0:
+                    self.cvals['nI1I2'][x2, x3] = 0.0
+                if abs(riq) < snr*uiq/2.0:
+                    self.cvals['nI1Q2'][x2, x3] = 0.0
+                if abs(rqi) < snr*uqi/2.0:
+                    self.cvals['nQ1I2'][x2, x3] = 0.0
+                if abs(rqq) < snr*uqq/2.0:
+                    self.cvals['nQ1Q2'][x2, x3] = 0.0
 
     def calU(self, z1, lags0, cpt):
         '''
@@ -359,6 +239,175 @@ class variable_carrier():
         z2 = z1[:lags0-cpt]*1.0
         z3 = z1[lags0+cpt:]*1.0
         return abs(np.sqrt(np.var(np.concatenate([z2, z3]))))
+
+
+'''
+                    2 Classes END
+Beginning of the functions used for fitting and such
+-------------------------------------------------------
+'''
+
+
+def xderiv(d2MAT, dx=1.0, axis=0):
+    '''
+    This  derivative is inaccurate at the edges.
+    Calculates a 3p derivative of a 1D, 2D matrix.
+    This does not require you to shift the xaxis by one half pt.
+    dx = distance between points
+    '''
+    if len(d2MAT.shape) > 1:
+        if axis == 1:
+            ''' Not tested yet could be faster than a matrix transpose'''
+            a2 = np.zeros([d2MAT.shape[0]+2, d2MAT.shape[1]])
+            a2[1:-1, :] = d2MAT
+            m1 = d2MAT - a2[:-2, :]
+            m2 = a2[2:, :] - d2MAT
+            dy = (m1+m2)/2.0
+            dy[0, :] = dy[1, :]
+            dy[-1, :] = dy[-2, :]
+        elif axis == 0:
+            a2 = np.zeros([d2MAT.shape[0], d2MAT.shape[1]+2])
+            a2[:, 1:-1] = d2MAT
+            m1 = d2MAT - a2[:, :-2]
+            m2 = a2[:, 2:] - d2MAT
+            dy = (m1+m2)/2.0
+            dy[:, 0] = dy[:, 1]
+            dy[:, -1] = dy[:, -2]
+        return dy/dx
+    else:
+        a2 = np.zeros([d2MAT.shape[0]+2])
+        a2[1:-1] = d2MAT
+        m1 = d2MAT - a2[:-2]
+        m2 = a2[2:] - d2MAT
+        dy = (m1+m2)/2.0
+        dy[0] = dy[1]
+        dy[-1] = dy[-2]
+        return dy/dx
+
+
+def find_nearest(someArray, value):
+    ''' This function helps to find the index corresponding to a value
+    in an array.
+    Usage: indexZero = find_nearest(myarray, 0.0)
+    returns: abs(myarray-value).argmin()
+    '''
+    idx = abs(someArray-value).argmin()
+    return idx
+
+
+def fitfunc(G, Tn, T, vc):
+    # def fitfunc(x, G, Tn, T, c):
+    '''
+    This contains the fitting equation, which i use to fit the
+    shot noise response.
+    returns: fit-value(x, ...)
+    '''
+    R = vc.dRm
+    E1 = (e*vc.I*R+h*vc.f)/(2*Kb*T)
+    E2 = (e*vc.I*R-h*vc.f)/(2*Kb*T)
+    Si = ((2*Kb*T/R) * (E1/np.tanh(E1) + E2/np.tanh(E2)))
+    return (vc.B*G*(Si * R**2 +
+                    4.0*Kb*T*R + 4.0*Kb*Tn*vc.Z0 *
+                    (vc.Z0**2+vc.Zopt*vc.Zopt)/(vc.Z0*vc.Z0+vc.Zopt*vc.Zopt)) *
+            (vc.Z0/((R+vc.Z0)*(vc.dRm+vc.Z0))))
+
+
+def fitfun2(params, vc, digi):
+    '''
+    req: params with G, Tn, T; and vc as variable carrier
+    return: fitting value or array
+    also set digi='D1' or digi='D2'
+    '''
+    T = params['T'].value
+    if digi == 'D1':
+        G = params['G1'].value
+        Tn = params['Tn1'].value
+        # T = params['T1'].value
+        vc.f = vc.f1
+    if digi == 'D2':
+        G = params['G2'].value
+        Tn = params['Tn2'].value
+        # T = params['T2'].value
+        vc.f = vc.f2
+    return fitfunc(G, Tn, T, vc)
+
+
+def ministuff(params, vc, measd, digi='D1'):
+    '''
+    req: params with G, Tn, T
+         I (current array or value) (Amps)
+         dRm (Resistance for this current)
+         digi='D1' or 'D2'
+         measured data (value or array)
+         crop values for example create with:
+         crop_within = find_nearest(I, -0.9e-6), find_nearest(I, 1.1e-6)
+         crop_outside = find_nearest(I, -19.5e-6), find_nearest(I, 19.5e-6)
+         crop = [crop_within, crop_outside]
+         This crop is used to cut data corresponding to the current values
+         i.e. to cut away the critical current part (from to (crop within))
+         also edges where the derivative and filter is inaccurate (crop outside)
+    returns: residuals*1e10;
+         (difference between measured and fitted data after it has been croped)
+    '''
+    SNfit = fitfun2(params, vc, digi)
+    SNfit[vc.crop[0][0]:vc.crop[0][1]] = 0
+    measd[vc.crop[0][0]:vc.crop[0][1]] = 0
+    SNfit[0:(vc.crop[1][0])] = 0
+    SNfit[vc.crop[1][1]:-1] = 0
+    measd[0:(vc.crop[1][0])] = 0
+    measd[vc.crop[1][1]:-1] = 0
+    return (measd-SNfit)*1e10
+
+
+def bigstuff(params, vc, pidx):
+    '''
+    return all 3 as one combined thing (shotnoise 1,2 and photon differences)
+    '''
+    data1 = vc.cPD1[pidx]*1.0
+    data2 = vc.cPD2[pidx]*1.0
+    snd1 = ministuff(params, vc, data1, digi='D1')
+    snd2 = ministuff(params, vc, data2, digi='D2')
+    ph1, ph2 = photoncal(params, pidx, vc)
+    phtd = abs(1-ph1/ph2) if abs(1-ph1/ph2) > 0.15 else 1.0
+    zpfmin = 1-ph1-ph2 if 1-ph1-ph2 > 0.0 else 0.0  # zpf: min 0.5 photons
+    return abs(snd1)*abs(snd2)*phtd + zpfmin*1e9
+    # return phtd
+
+
+def photoncal(params, pidx, vc):
+    '''
+    calculates the difference in photon numbers at the hemt input
+    for a current bias of Ib0 (0 uA) and after the amplifier photons and temp
+    photons are subtracted.
+    '''
+    d1 = vc.cPD1[pidx]*1.0
+    d2 = vc.cPD2[pidx]*1.0
+    p1 = d1[vc.Ib0]/(h*vc.f1*vc.B*params['G1'].value)
+    p2 = d2[vc.Ib0]/(h*vc.f2*vc.B*params['G2'].value)
+    Amp1 = Kb*params['Tn1'].value/(h*vc.f1)
+    Amp2 = Kb*params['Tn2'].value/(h*vc.f2)
+    Phot1 = p1 - Amp1  # -Ent1
+    Phot2 = p2 - Amp2  # -Ent2
+    phT1 = blackbodyrad(vc.f1, params['T'].value)  # est phot num from Temp
+    phT2 = blackbodyrad(vc.f2, params['T'].value)
+    # Need to make sure that the number of photons are strictly more than 0.5
+    # since that is due to the zpf! thus Phot1+Phot2 - 1 > 0
+    return Phot1-phT1, Phot2-phT2
+    # return abs(1-Phot1/Phot2) if abs(1-Phot1/Phot2) > 0.15 else 1.0
+
+
+def blackbodyrad(f, T):
+    '''
+    returns the number of photons expected for a certain temperature present
+    Energy of photons(f,T): (2*h*f**3)/(c**2)*1/(np.e(h*f/(Kb*T))-1)
+    (see Plank's law) units of Power/angle/meter^2/unit Bandwidth
+    4*pi/c
+    '''
+    spectral_radiance = 2*f*f/(c*c*(np.e**(h*f/(Kb*T))-1))
+    area = 7*4e-4  # 7x4 cm^2 chip
+    return spectral_radiance*area
+    # spectral_phot_density = spectral_radiance*4*np.pi/c
+    # return spectral_phot_density
 
 
 def DoSNfits(vc, plotFit=False):
@@ -397,34 +446,36 @@ def DoSNfits(vc, plotFit=False):
     vc.load_and_go()
     vc.calc_diff_resistance()
     # create crop vector for the fitting
-    crop_within = find_nearest(vc.I, -1.3e-6), find_nearest(vc.I, 1.3e-6)
+    crop_within = find_nearest(vc.I, -0.4e-6), find_nearest(vc.I, 0.5e-6)
     crop_outside = find_nearest(vc.I, -19e-6), find_nearest(vc.I, 19e-6)
     vc.crop = [crop_within, crop_outside]
     # create fitting parameters
     params = Parameters()
     params.add('Tn1', value=3.7, vary=True, min=2.0, max=4.0)
     params.add('G1', value=3.38e7, vary=True, min=1e6, max=1e9)
-    params.add('T1', value=0.05, vary=False, min=0.01, max=0.25)
     params.add('Tn2', value=3.7, vary=True, min=2.0, max=4.0)
     params.add('G2', value=3.38e7, vary=True, min=1e6, max=1e9)
-    params.add('T2', value=0.05, vary=False, min=0.01, max=0.25)
+    # params.add('T1', value=0.05, vary=False, min=0.01, max=0.25)
+    # params.add('T2', value=0.05, vary=False, min=0.01, max=0.25)
+    params.add('T', value=vc.Texp, vary=False, min=0.007, max=0.38)
     data1 = vc.cPD1*1.0
     data2 = vc.cPD2*1.0
-    for pidx in range(len(data1)):
+    for pidx in range(vc.cPD1.shape[0]):
         '''
         scales Voltage_trace[selected power] to Volts
         obtains differential Resistance Rm
         fits selected data set
         records corresponding fit results into SN_r class values
         '''
-        vc.dRm = vc.dIVlp[pidx]    # select dRm
+        vc.dRm = vc.dIVlp[pidx]  # select dRm
         result = minimize(ministuff, params, args=(vc, data1[pidx]*1.0, 'D1'))
-        result = minimize(ministuff, result.params,
-                          args=(vc, data2[pidx]*1.0, 'D2'))
+        result = minimize(ministuff,
+                          result.params, args=(vc, data2[pidx]*1.0, 'D2'))
 
         # now fit all of them together:
-        result.params['T1'].vary = True
-        result.params['T2'].vary = True
+        # result.params['T1'].vary = True
+        # result.params['T2'].vary = True
+        result.params['T'].vary = True
         result = minimize(bigstuff, result.params, args=(vc, pidx))
         print 'RF power', vc.d2.lin[pidx]
         print report_fit(result)
@@ -432,12 +483,12 @@ def DoSNfits(vc, plotFit=False):
         SNr.Tn1del.append(result.params['Tn1'].stderr)
         SNr.G1.append(result.params['G1'].value)
         SNr.Tn1.append(result.params['Tn1'].value)
-        SNr.T1.append(result.params['T1'].value)
         SNr.G2del.append(result.params['G2'].stderr)
         SNr.Tn2del.append(result.params['Tn2'].stderr)
         SNr.G2.append(result.params['G2'].value)
         SNr.Tn2.append(result.params['Tn2'].value)
-        SNr.T2.append(result.params['T2'].value)
+        SNr.T.append(result.params['T'].value)
+        SNr.Tdel.append(result.params['T'].stderr)
         if plotFit is True:
             plotSNfit(result, vc, pidx, 'D1')
         if plotFit is True:
