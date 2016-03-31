41c8fa78938cb36fc5f5e38dc5b3b2d020e6be0a
Ben <benh.schneider@gmail.com>
2016-03-31 16:51:32 +0200

    testing different ways of filtering sidebands

--- FFT_filters/digFiltSim.py
+++ FFT_filters/digFiltSim.py
@@ -12,12 +12,12 @@ t = np.linspace(0, N*T, N)  # time
 fstep = 1.0/(N*T)
 
 s1 = 1.0
-#s2 = 0.0
+s2 = 2.0
 
 f1 = 1000
 Lo = 187
 f2 = f1 + 2*Lo
-# phi = 10.0
+phi = 0.0
 
 
 def digi(signal, f1):
@@ -33,25 +33,25 @@ signal = sig1 + sig2
 Isig, Qsig = digi(signal, f1)
 Isig1, Qsig1 = digi(sig1, f1)
 
-fftI = np.fft.fftshift(np.fft.fft(Isig))
-fftQ = np.fft.fftshift(np.fft.fft(Isig))
-freq = np.fft.fftshift(np.fft.fftfreq(N, d=T))
+csig = 1j*Isig + Qsig
 
-fftI1 = np.fft.fftshift(np.fft.fft(Isig1))
-fftQ1 = np.fft.fftshift(np.fft.fft(Isig1))
+fftcsig = np.fft.fftshift(np.fft.fft(csig))
+freq = np.fft.fftshift(np.fft.fftfreq(N, d=T))
+# fftI = np.fft.fftshift(np.fft.fft(Isig))
+# fftQ = np.fft.fftshift(np.fft.fft(Qsig))
+# freq = np.fft.fftshift(np.fft.fftfreq(N, d=T))
+# fftI1 = np.fft.fftshift(np.fft.fft(Isig1))
+# fftQ1 = np.fft.fftshift(np.fft.fft(Isig1))
 
 
 def LPfft(sinput, lpfreq, T):
     Nlp = int(lpfreq*len(sinput)*T)
-    # fftlp = np.fft.fftshift(np.fft.fft(Isig))
-    # fftlp[0:(N/2-Nlp)] = 0
-    # fftlp[(N/2+Nlp):-1] = 0
     fftlp = np.fft.fft(sinput)  # 0,1,2,3,-3,-2,-1
     fftlp[Nlp+1:-Nlp] = 0.0
     return fftlp
 
 
-def killsideband(Idata, killup=True):
+def killsideband(sinput, killup=True):
     smid = int(len(sinput)/2) + 1
     if killup is True:
         sinput[smid:-1] = 0.0
@@ -62,6 +62,10 @@ def killsideband(Idata, killup=True):
 
 plt.close('all')
 plt.ion()
+plt.figure()
+plt.plot(freq, np.abs(fftcsig)/(N-1))
+plt.plot(freq, np.angle(fftcsig))
+
 # plt.figure(1)
 # plt.plot(t, sig1)
 # plt.title('Sig1')
@@ -74,27 +78,26 @@ plt.ion()
 # plt.figure(4)
 # plt.plot(t, Qsig)
 # plt.title('Qsig')
-plt.figure(5)
-plt.plot(freq, np.abs(fftI-fftI1)/(N-1))
-plt.plot(freq, np.abs(fftI1)/(N-1))
-plt.title('realfftI')
-plt.figure(6)
-plt.plot(freq, np.angle(fftI-fftI1)/(N-1))
-plt.plot(freq, np.angle(fftI1)/(N-1))
-plt.title('imagfftI')
-
-lpI = LPfft(Isig, T, 4000)
-lpQ = LPfft(Qsig, T, 4000)
+# plt.figure(5)
+# plt.plot(freq, np.abs(fftI-fftI1)/(N-1))
+# plt.plot(freq, np.abs(fftI1)/(N-1))
+# plt.title('realfftI')
+# plt.figure(6)
+# plt.plot(freq, np.angle(fftI-fftI1)/(N-1))
+# plt.plot(freq, np.angle(fftI1)/(N-1))
+# plt.title('imagfftI')
+# lpI = LPfft(Isig, T, 4000)
+# lpQ = LPfft(Qsig, T, 4000)
 # lpI = killsideband(lpI, killup=True)
 # lpQ = killsideband(lpQ, killup=False)
-plt.figure(7)
-plt.plot(freq, np.fft.fftshift(np.abs(lpI)/(N-1)))
-plt.plot(freq, np.fft.fftshift(np.angle(lpI)))
-plt.title('lpfftI')
-plt.figure(8)
-plt.plot(t, np.fft.ifft(lpI))
-plt.plot(t, np.fft.ifft(lpQ))
-plt.show()
+# plt.figure(7)
+# plt.plot(freq, np.fft.fftshift(np.abs(lpI)/(N-1)))
+# plt.plot(freq, np.fft.fftshift(np.angle(lpI)))
+# plt.title('lpfftI')
+# plt.figure(8)
+# plt.plot(t, np.fft.ifft(lpI))
+# plt.plot(t, np.fft.ifft(lpQ))
+# plt.show()
 
 
 # resultmat = np.zeros([N, 2])
